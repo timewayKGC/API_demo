163fe8b019ed5e2d05eae3839b151fc8fb46778a
timewayKCG <dwayne.sze@gmail.com>
2020-11-16 00:11:32 -0500

    adding python_repo file with api call

---
+++ python_repos.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+response_dict = r.json()
+
+# Process results.
+print(response_dict.keys)
