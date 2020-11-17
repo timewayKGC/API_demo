e7ec75d04165ccf1bec45582b3125fc18101af35
timewayKCG <dwayne.sze@gmail.com>
2020-11-16 23:21:15 -0500

    changed to other api

--- python_repos.py
+++ python_repos.py
@@ -1,10 +1,17 @@
 import requests
 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+url = 'https://hacker-news.firebaseio.com/v0/item/9884165.json'
 r = requests.get(url)
+
 print("Status code:", r.status_code)
 
-response_dict = r.json()
-
-# Process results.
-print(response_dict.keys)
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # Make a separate API call for each submission.
+    url = ('https://hacker-news.firebaseio.com/v0/item/' +
+    str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.status_code)
+    response_dict = submission_r.json()
